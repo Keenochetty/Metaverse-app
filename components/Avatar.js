727456a273bbee79919fb658edbc687662356ef5
Keenochetty <86490210+Keenochetty@users.noreply.github.com>
2021-12-18 23:25:54 +0200

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ import { useMoralis } from 'react-moralis';
 function Avatar({username, logoutOnPress}) {
     const { user, logout } = useMoralis();
     return <Image className="rounded-full bg-black cursor-pointer hover:opacity-75 " 
-    src={`https://avatars.dicebear.com/api/adventurer/${username || user.get("username") }.svg`} 
+    src={`https://avatars.dicebear.com/api/adventurer-neutral/${username || user.get("username") }.svg`} 
     onClick={() => logoutOnPress && logout() } layout="fill" />;
         
     
